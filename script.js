57d5dfe1862adafae647694b1b84d4d9f614a133
taropuffgurl <42387889+taropuffgurl@users.noreply.github.com>
2020-12-21 14:36:09 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("time").innerHTML = date();
+
+function date() {
+    var d = new Date(),
+      days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      months =['January', 'February', 'March','April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return days[d.getDay()] + ',  ' + months[d.getMonth()] + ' ' + d.getDate();
+   }
+
